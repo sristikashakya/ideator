d0e07c1496457d394a35fc6777102f944a58ac23
sristikashakya123@gmail.com <sristikashakya123@gmail.com>
2017-04-20 15:37:45 +0545

    creating a model

---
+++ db/migrate/20170420093657_create_ideas.rb
@@ -0,0 +1,10 @@
+class CreateIdeas < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ideas do |t|
+		t.text :description
+		t.string :author
+
+		t.timestamps null: false
+    end
+  end
+end
