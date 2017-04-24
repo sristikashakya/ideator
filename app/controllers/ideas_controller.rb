38dabee88e720d6ff607c89650860cd17e4a7900
sristikashakya123@gmail.com <sristikashakya123@gmail.com>
2017-04-24 12:38:05 +0545

    validate user input

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,11 +5,21 @@ class IdeasController < ApplicationController
 	
 	def create
 		@idea=Idea.create(idea_params)
+		if @idea.valid?
+			#implement later
+		else
+			#implement later
+		end
 		redirect_to root_path
 	end
 	def edit
 		@idea=Idea.find(params[:id])
-	end	
+		if @idea.update(idea_parms)
+			redirect_to root_path
+		else
+			redirect_to edit_idea_path(params[:id])
+		end	
+	end
 	private
 	def idea_params
 		  params.require(:idea).permit(:description, :author)
