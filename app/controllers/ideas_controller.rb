183918902500380d40e09d494638be4c65d10fd0
sristikashakya123@gmail.com <sristikashakya123@gmail.com>
2017-04-24 12:27:40 +0545

    updating ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,6 +7,9 @@ class IdeasController < ApplicationController
 		@idea=Idea.create(idea_params)
 		redirect_to root_path
 	end
+	def edit
+		@idea=Idea.find(params[:id])
+	end	
 	private
 	def idea_params
 		  params.require(:idea).permit(:description, :author)
